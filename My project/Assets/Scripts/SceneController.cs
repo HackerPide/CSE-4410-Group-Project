f03c37211d3868b6ee1364ee0c55d14dab9aa935
scottrenfrow01 <77753517+scottrenfrow01@users.noreply.github.com>
2025-03-11 14:43:46 -0700

    Doubling enemies every round

--- My project/Assets/Scripts/SceneController.cs
+++ My project/Assets/Scripts/SceneController.cs
@@ -5,23 +5,22 @@ using UnityEngine;
 public class SceneController : MonoBehaviour {
     [SerializeField] GameObject enemyPrefab;
     private GameObject enemy;
-    int numEnemy = 5;
+    int numEnemy = 2;
     public int roundNum = 1;
 
     void Start() {
     }
 
     void Update() {
-        if (roundNum == 1) {
-            for (int i = 0; i < numEnemy; i++)
-            {
-                enemy = Instantiate(enemyPrefab) as GameObject;
-                enemy.transform.position = new Vector3(5, 1, 0);
-                float angle = Random.Range(0, 360);
-                enemy.transform.Rotate(0, angle, 0);
-            }
-            roundNum++;
-            numEnemy *= 2;
+        for (int i = 0; i < numEnemy; i++)
+        {
+            enemy = Instantiate(enemyPrefab) as GameObject;
+            enemy.transform.position = new Vector3(5, 1, 0);
+            float angle = Random.Range(0, 360);
+            enemy.transform.Rotate(0, angle, 0);
         }
+        roundNum++;
+        numEnemy *= 2;
+        
     }
 }
